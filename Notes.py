0e91aad6b1a832b134389f4f7ef07a5baf361080
JesseBenetti <jessebenett.au@gmail.com>
2024-12-18 06:44:48 +1100

    first commit

---
+++ Notes.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 06:22:52 2024
+
+@author: jesse
+"""
+
+# %% Introdução à linguagem Python
+
+# %% Structura and basics - page 23 to 24
+a = 2
+b = 3
+s = a + b
+print("A soma vale ", s)
+
+# %% Extending the code to a new line
+
+x = 8.7
+print('O valor de x é ',
+      x)
+
+# %% Variables
+
+n = 2
+pi = 3.14
+texto = 'Exemplo'
+print(n, pi, text)
+print(type(n), type(pi), type(text))
